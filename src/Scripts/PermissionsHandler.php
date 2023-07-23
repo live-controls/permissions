4871cc95a9713b2781c93e573be48e34ad3d953a
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-07-23 09:43:05 -0300

    Added UserGroups to permissions

--- src/Scripts/PermissionsHandler.php
+++ src/Scripts/PermissionsHandler.php
@@ -3,7 +3,9 @@
 namespace LiveControls\Permissions\Scripts;
 
 use App\Models\User;
+use Exception;
 use LiveControls\Permissions\Models\UserPermission;
+use LiveControls\Groups\Models\UserGroup;
 
 class PermissionsHandler{
     private array $permissions = [];
@@ -37,11 +39,19 @@ class PermissionsHandler{
     }
 
     public function check(User $user = null):bool{
+        if(!in_array(HasGroups::class, class_uses_recursive(User::class))){
+            throw new Exception('HasGroups trait is missing in User class!');
+        }
+
         if(is_null($user)){
             $user = auth()->user();
         }
 
-
+        if(class_exists('\LiveControls\Groups\GroupsServiceProvider', false)){
+            $groups = $user->groups;
+        }else{      
+            $groups = null;
+        }
         foreach($this->permissions as $permission){
             $perm = UserPermission::where('key', '=', $permission)->first();
             if(is_null($perm)){
@@ -51,8 +61,27 @@ class PermissionsHandler{
             if($perm->users()->where('users.id', '=', $user->id)->exists()){
                 return true;
             }
+            foreach($groups as $group){
+                if($perm->groups()->where('livecontrols_user_groups.id', '=', $group->id)->exists()){
+                    return true;
+                }
+            }
         }
 
+        return false;
+    }
+
+    public function checkGroup(UserGroup $group):bool{
+        foreach($this->permissions as $permission){
+            $perm = UserPermission::where('key', '=', $permission)->first();
+            if(is_null($perm)){
+                //Ignore if permission was not found
+                continue;
+            }
+            if($perm->groups()->where('user_groups.id', '=', $group->id)->exists()){
+                return true;
+            }
+        }
         return false;
     }
 }
